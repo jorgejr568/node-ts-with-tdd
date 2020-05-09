3ae9f73045111715c186e4e9a54db7213db3211c
Jorge Junior <jorge.junior.568@live.com>
2020-05-09 11:55:54 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -14,6 +14,11 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const collection = MongoHelper.getCollection('accounts')
+    await collection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     const sut = makeSut()
 
