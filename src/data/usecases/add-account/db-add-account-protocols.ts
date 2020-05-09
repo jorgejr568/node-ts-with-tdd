2d71a76a61bc45a6430da4b37dfd2b7cb856ddcc
Jorge Junior <jorge.junior.568@live.com>
2020-05-09 10:37:11 -0300

    feat: ensure AddAccountRepository is called with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/account-model'
 export * from '../../protocols/encrypter'
+export * from '../../protocols/add-account-repository'
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,19 +1,18 @@
-import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-account-protocols'
+import { AddAccount, AddAccountModel, AccountModel, Encrypter, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    const hashedPassword = await this.encrypter.encrypt(account.password)
-    return new Promise(resolve => resolve({
-      id: 'valid_id',
-      name: 'valid_name',
-      email: 'valid_email',
-      password: hashedPassword
-    }))
+    account.password = await this.encrypter.encrypt(account.password)
+
+    const accountModel = await this.addAccountRepository.add(account)
+    return new Promise(resolve => resolve(accountModel))
   }
 }
