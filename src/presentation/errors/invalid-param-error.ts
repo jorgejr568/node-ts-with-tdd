5784c2e15c835c959043e8035381cd83a6678597
Jorge Junior <jorge.junior.568@live.com>
2020-05-10 12:48:24 -0300

    refactor: make param exceptions to declare paramName

--- src/presentation/errors/invalid-param-error.ts
+++ src/presentation/errors/invalid-param-error.ts
@@ -1,6 +1,9 @@
 export class InvalidParamError extends Error {
+  private readonly param: string
+
   constructor (paramName: string) {
     super(`Invalid param: ${paramName}`)
     this.name = 'InvalidParamError'
+    this.param = paramName
   }
 }
--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,9 @@
 export class MissingParamError extends Error {
+  private readonly param: string
+
   constructor (paramName: string) {
     super(`Missing param: ${paramName}`)
     this.name = 'MissingParamError'
+    this.param = paramName
   }
 }
