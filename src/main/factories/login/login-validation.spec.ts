1af100e59682e5250f63fc3c0192e7859b6894cc
Jorge Junior <jorge.junior.568@live.com>
2020-05-10 18:11:40 -0300

    feat: login validation created

---
+++ src/main/factories/login/login-validation.spec.ts
@@ -0,0 +1,21 @@
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { RequiredFieldsValidation } from '../../../presentation/helpers/validators/required-fields-validation'
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { makeLoginValidation } from './login-validation'
+
+jest.mock('../../../presentation/helpers/validators/validation-composite')
+
+describe('Login Validation', () => {
+  test('Should construct ValidationComposite with correct validations', async () => {
+    makeLoginValidation()
+    const validations: Validation[] = []
+    for (const field of ['email', 'password']) {
+      validations.push(new RequiredFieldsValidation(field))
+    }
+    validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,10 +1,10 @@
-import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../infra/crypt/bcrypt-adapter'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-import { Controller } from '../../presentation/protocols'
-import { LogControllerDecorator } from '../decorators/log'
-import { LogErrorMongoRepository } from '../../infra/db/mongodb/log-error/log-error'
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../../infra/crypt/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log'
+import { LogErrorMongoRepository } from '../../../infra/db/mongodb/log-error/log-error'
 import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
