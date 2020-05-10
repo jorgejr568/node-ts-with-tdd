30419829f79bb33e64eed4ed90fc99b40e581268
Jorge Junior <jorge.junior.568@live.com>
2020-05-10 15:13:52 -0300

    feat: ensure loginController calls authentication and emailValidator with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,25 +1,32 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { badRequest, okResponse } from '../../helpers'
+import { badRequest, okResponse, serverError } from '../../helpers'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { EmailValidator } from '../../protocols/email-validator'
+import { Authentication } from '../../../domain/usecases/authentication'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly authentication: Authentication
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, authentication: Authentication) {
     this.emailValidator = emailValidator
+    this.authentication = authentication
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['email', 'password']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
-    }
+    try {
+      const requiredFields = ['email', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+      }
+      const { email, password } = httpRequest.body
+      const isValidEmail = this.emailValidator.isValid(email)
+      if (!isValidEmail) return badRequest(new InvalidParamError('email'))
 
-    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValidEmail) return badRequest(new InvalidParamError('email'))
-    return new Promise(resolve => resolve(okResponse({
-      foo: 'bar'
-    })))
+      const token = await this.authentication.auth(email, password)
+      return new Promise(resolve => resolve(okResponse({ token })))
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
--- src/presentation/helpers/server-error.ts
+++ src/presentation/helpers/server-error.ts
@@ -1,7 +1,7 @@
 import { HttpResponse } from '../protocols/http'
 import { ServerError } from '../errors/server-error'
 
-export const serverError = (error: Error): HttpResponse => {
+export const serverError = (error?: Error): HttpResponse => {
   return {
     statusCode: 500,
     body: new ServerError(error)
