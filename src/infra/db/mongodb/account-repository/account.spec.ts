e6dad684e8bf2848bd619d23973244a82902b46a
Jorge Junior <jorge.junior.568@live.com>
2020-05-10 21:09:00 -0300

    feat: ensure MongoAccountRepository returns account or null depeding on the result of query

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,9 +1,16 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
 
 const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository()
 }
+
+const makeAccountData = (): AddAccountModel => ({
+  name: 'valid_name',
+  email: 'valid_email',
+  password: 'hashed_password'
+})
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -22,16 +29,28 @@ describe('Account Mongo Repository', () => {
   test('Should return an account on success', async () => {
     const sut = makeSut()
 
-    const account = await sut.add({
-      name: 'valid_name',
-      email: 'valid_email',
-      password: 'hashed_password'
-    })
+    const account = await sut.add(makeAccountData())
 
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('valid_name')
     expect(account.email).toBe('valid_email')
     expect(account.password).toBe('hashed_password')
+  })
+
+  test('Should return null when cannot find the account', async () => {
+    const sut = makeSut()
+    const account = await sut.load(makeAccountData().email)
+
+    expect(account).toBe(null)
+  })
+
+  test('Should return an Account on success load account', async () => {
+    const sut = makeSut()
+    const accountData = makeAccountData()
+    const accountModel = await sut.add(accountData)
+    const account = await sut.load(accountData.email)
+
+    expect(account).toEqual(accountModel)
   })
 })
